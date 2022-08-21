ad14737e6078a35265670ce591903ab81be908ae
bleck che roy wandji <bleckbeckham@yahoo.com>
2022-08-21 18:35:26 +0500

    my landmark notes2

---
+++ landmark2.sh
@@ -0,0 +1,138 @@
+#!/bin/sh
+Landmark-SS -         Paypal:
+=============================  
+    Develops, Tests, Builds, deploys & monitors applications 
+    Applications are the output from Softwares  
+  
+  Releases:
+    Release 1  = version 1      = 1000 lines of code  
+       echo "Welcome to paypal"  
+
+    Release 2  = version 2  = 2000 lines of code  
+       echo "Welcome to paypal"
+       echo "paypal your convenient payment gateway"
+
+    Release 3  = version 3  = 1000 lines of code  
+       echo "Paypal the best payment gateway "
+
+    Release n  = version 4
+
+ Rollout   -- version1 --> version2--->version3   
+ RollBack  -- version3 --> version2---
+
+Versioning in Software Development:
+===================================
+Version control systems:
+  Local 
+  Centralised 
+  Distributed
+     Git 
+
+
+Fintech Customers/clients : 
+   visa/paypal/mastercard/ae 
+
+Version Control systems:
+  - Local [using your own computer/system]
+       Developers write and save codes in their local-Computers 
+  - Centralised
+  - Distributed
+Git and GitHub
+==============
+GIT: DVCs 
+  Distributed Version Control system 
+  - Git manage data as snapshots.
+  - It takes a picture of what all your files look like 
+     at that moment and stores 
+    a reference to that snapshot.
+  Benefits: Nearly every operation is local
+            git has data intergrity
+            Git generally only adds data
+
+SCM = Source Code management
+=======================-====  
+
+other DVCs:
+SVN --> Sub Version
+CVS --> Concurrent Version system
+TFS --> Team Foundation Server
+Git --> 95% plus of companies are using GIT   
+
+Git --> Distributed Version Control systems. DVCS
+Source Code Management = SCM:
+        GitHub - MS
+        GitLab
+        BitBucket (Stash) - A
+
+IQ: As a DevOps Engineer, what are your roles & responsilities in Git?
+Answer:
+  1. Ensure that Developers environments are configured and secured 
+     Create an enabling environment for Developers
+
+  2. Project Onboarding 
+       Create organisations where applicable 
+       Create repositories in SCM [GitHub] 
+       Create teams in SCM and assign members[Developers, etc.] 
+ 
+  3. visa
+
+Project:
+  For this sprint you are to onboard our new clients beginning with:
+    1. VISA   
+
+agile environment
+=================
+ A sprint is a Development cycle of about 10wd ---
+ Tasks to onboard VISA
+ ====================
+   1. Create a github for account where applicable
+        https://github.com/
+            email address  = Lucbuea@gmail.com   
+            username: Lucbuea 
+            password: admin123   
+   
+   2. Create organisation in your GitHub account for VISA
+        url = https://github.com/landmarktech29   
+   
+   4. Create TEAMS and add members 
+       Team1 url = https://github.com/orgs/landmarktech29/teams/visa-team
+       Team2 url = https://github.com/orgs/landmarktech29/teams/visa-dev-team
+        legah2045  elieolu   CKay925  Vin-Iyke DanjumaAmeh esejason AnnZeran
+
+   3. Create repositories in the GitHub account 
+       repo url = https://github.com/landmarktech29/visa
+
+   5. Assign roles to Team members
+      added external collaborators 
+
+        Provide/Grant repo access to the team
+         least privileges to team members   
+
+   6. Ensure that Git and relevant IDEs are installed by Developers  
+--------------------------------------------------------------------
+All team members should own a github free account    
+   Tita, Dominion, Olu, Choima, Mercy Esia 
+
+TeamLead = Mercy   
+  Create Team and send join requests to her team menbers 
+
+----------------------------------------------------------------------
+
+
+Git Installations
+-----------------
+Pre Requisite Software Download/ Registration :
+Install git in Windows   :  
+  Install git bash @ https://git-scm.com/downloads
+
+Install in RedHAT Linux servers:
+    sudo yum install git  -y 
+
+mkdir tesla 
+cd tesla 
+
+
+https://github.com/landmarktech29
+
+
+IQ: What do you use for Versioning in your projects?? Git/ GitHub
